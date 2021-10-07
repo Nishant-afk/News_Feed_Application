ca1daf867183db439da935df15c612216a75648f
Nishant-afk <75487910+Nishant-afk@users.noreply.github.com>
2021-10-07 20:30:00 +0530

    Create myfooter.js

---
+++ myfooter.js
@@ -0,0 +1,62 @@
+
+console.log("footer is loaded");
+
+function disp_changes(myurl){
+    let newsdisp = document.getElementById('newsdisp');
+
+    //Create a ajax get request
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', myurl, true);
+    //xhr.getResponseHeader('Content-type', 'application/json');
+    xhr.onload = function () {
+        if (this.status === 200) {
+
+            let json = JSON.parse(this.responseText);
+            let articles = json.articles;
+            let newshtml = "";
+            console.log(json);
+            articles.forEach(function (element) {
+                news = '<div class="col-lg-4 col-md-6 col-sm-6 d-flex">' +
+                '<div class="card flex-fill">' +
+                '<img class="card-img-top img-fluid" src="' +
+                element["urlToImage"] +
+                '" alt="Card image cap">' +
+                '<div class ="card-body flex-fill">' +
+                '<h4 class ="card-title"><a href=" ' +
+                element["url"] +
+                '" target="_blank" rel="noopener noreferrer">' +
+                element["title"] +
+                '</a></h4>' +
+                '<p class ="card-text"> ' +
+                element["description"] +
+                '</p> ' +
+                '<div class="card-footer text-muted"> ' +
+                element["publishedAt"] +
+                '</div> ' +
+                '</div>' +
+                '</div> ' +
+                '</div>';
+                //console.log(articles[news]);
+
+                newshtml += news;
+            });
+            newsdisp.innerHTML = newshtml;
+        }
+        else {
+            console.log("Some error occured");
+        }
+    }
+
+    xhr.send()
+}
+
+function UK(){
+    url = 'https://newsapi.org/v2/top-headlines?country=gb&apiKey=455a87c940c341c08bdc416a2ec50bc0';
+    disp_changes(url);
+}
+
+function US(){
+
+    url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=455a87c940c341c08bdc416a2ec50bc0';
+    disp_changes(url);
+}
